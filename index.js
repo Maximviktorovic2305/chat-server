1298d415379fea71afa18073cd47dc44232c49c0
Maximviktorovic2305 <Maximviktorovic123@ya.ru>
2024-04-16 10:10:52 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import cors from "cors";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+app.use(cors({ origin: "" }));
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+   cors: {
+      origin: "http://localhost:7750",
+      methods: ["GET", "POST"],
+   },
+});
+
+const users = [];
+
+io.on("connection", (socket) => {
+   console.log(`User ${socket.id} connect`);
+
+   socket.on("message", (data) => {
+      io.emit("response", data);
+   });
+
+   socket.on("newUser", (data) => {
+      users.push(data);
+      io.emit("responseNewUser", users);
+   });
+
+   socket.on("typing", (data) => socket.broadcast.emit("responseTyping", data));
+
+   io.on("disconnect", () => {
+      console.log(`User ${socket.id} disconnect`);
+   });
+});
+
+server.listen(7000, () => {
+   console.log("Server is running");
+});
